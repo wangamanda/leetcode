9d05abefe03f8808cee6ec21b8278824aac113b5
Xin Wang <wangamanda.lu@gmail.com>
2014-04-15 17:43:34 -0400

    circle list

---
+++ LinkedList/Circle.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class Circle{
+	ListNode n1 = new ListNode(1);
+	ListNode n2 = new ListNode(2);
+	ListNode n3 = new ListNode(3);
+	ListNode n4 = new ListNode(4);
+	ListNode n5 = new ListNode(5);
+	ListNode n6 = new ListNode(6);
+	n1.next = n2;
+	n2.next = n3;
+	n3.next = n4;
+	n4.next = n5;
+	n5.next = n6;
+
+
+}
