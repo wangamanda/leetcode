c9067475ce500425fdc1f2275b9730ebaa52892f
Xin Wang <wangamanda.lu@gmail.com>
2014-04-24 22:46:32 -0400

    partition list

---
+++ LinkedList/PartitionList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class PartitionList{
+	ListNode n1 = new ListNode(1);
+	ListNode n2 = new ListNode(4);
+	ListNode n3 = new ListNode(3);
+	ListNode n4 = new ListNode(2);
+	ListNode n5 = new ListNode(5);
+	ListNode n6 = new ListNode(2);
+	
+	public static void main(String[] args){
+	
+	}
+}
