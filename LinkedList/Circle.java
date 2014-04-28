585c8a5a58195d5b91970f33176a18142e6a6057
Xin Wang <wangamanda.lu@gmail.com>
2014-04-28 11:44:43 -0400

    circle

--- LinkedList/Circle.java
+++ LinkedList/Circle.java
@@ -2,18 +2,27 @@ package LinkedList;
 
 public class Circle{
 
+	public static ListNode n1 = new ListNode(1);
+	public static ListNode n2 = new ListNode(2);
+	public static ListNode n3 = new ListNode(3);
+	public static ListNode n4 = new ListNode(4);
+	public static ListNode n5 = new ListNode(5);
+	public static ListNode n6 = new ListNode(6);
 	public static void main(String[] args){
-		ListNode n1 = new ListNode(1);
-		ListNode n2 = new ListNode(2);
-		ListNode n3 = new ListNode(3);
-		ListNode n4 = new ListNode(4);
-		ListNode n5 = new ListNode(5);
-		ListNode n6 = new ListNode(6);
 		n1.next = n2;
 		n2.next = n3;
 		n3.next = n4;
 		n4.next = n5;
 		n5.next = n6;
+		n6.next = n3;
+
+		ListNode head = FindHead(n1);
+		System.out.println(head.val+"");
+	}
+
+	public static ListNode FindHead(ListNode n){
+		ListNode res = n;
+
 	}
 	
 }
