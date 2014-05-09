ba6c945c6407f11887ed106233f003c7a3395500
Xin Wang <wangamanda.lu@gmail.com>
2014-05-08 22:00:04 -0400

    jump game

---
+++ JumpGame.java
@@ -0,0 +1,36 @@
+public class JumpGame{
+	public static void main(String[] args){
+		int[] A = {2, 3, 1, 1, 4};
+		boolean res = canJump(A);
+		if(res == true){
+			System.out.println("YES");
+		}else{
+			System.out.println("NO");
+		}
+	}
+
+	public static boolean canJump(int[] A){
+		boolean r = jump(A, 0);
+		return r;
+	}
+
+	public static boolean jump(int[] A, int n){
+		if(A.length-1 <= n){
+			return true;
+		}
+
+		if(n < A.length-1 && A[n] == 0){
+			return false;
+		}
+
+		boolean b= false;
+		for(int i = 1 ; i <= A[n] ; i ++ ){
+			b = jump(A, n+i);
+			if(b == true){
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
