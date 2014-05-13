fc58635ce7063f77fa7a668ecf8981911fbcf830
Xin Wang <wangamanda.lu@gmail.com>
2014-05-12 23:04:44 -0400

    atoi

---
+++ ArrayString/atoi.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class atoi{
+	public static void main(String[] args){
+		String s = "489999";
+		int n = Integer.parseInt(s);
+		System.out.println(s);
+		System.out.println(n+"");
+	}
+}
